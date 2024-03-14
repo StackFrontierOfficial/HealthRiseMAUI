2bdc9674e52c105cd0641639b7dbbf36811c4eb5
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:36:57 +0530

    Initial Commit

---
+++ HealthRiseMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthRiseMAUI
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ HealthRiseMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthRiseMAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
